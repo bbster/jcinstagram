1d6d61ce55ee8269390d2877b40dfe1696f2f67a
thecook <bbster12@gmail.com>
2019-10-26 20:17:46 +0900

    Image views comment 쿼리 수정

--- images/views.py
+++ images/views.py
@@ -24,7 +24,8 @@ class ListAllComment(viewsets.ModelViewSet):
 
     def get(self, request, format=None):
 
-        all_comment = Comment.objects.all()  # 모든 이미지 출력
+        user_id = request.user.id
+        all_comment = Comment.objects.filter(creator=user_id)  # 모든 이미지 출력
         serializer = serializers.CommentSerializer(all_comment, many=True)
 
         return Response(data=serializer.data)
@@ -37,40 +38,7 @@ class ListAllLikes(viewsets.ModelViewSet):
 
     def get(self, request, format=None):
         all_like = Like.objects.all()  # 모든 이미지 출력
+
         serializer = serializers.CommentSerializer(all_like, many=True)
 
         return Response(data=serializer.data)
-
-
-# class Feed(viewsets.ModelViewSet):
-#
-#     serializer_class = ImageSerializer
-#     queryset = Image.objects.all()
-#
-#     def get(self, request, format=None):
-#
-#         user = request.queryset.user()
-#
-#         following_users = user.following.all()
-#
-#         image_list = []
-#
-#         for following_user in following_users:
-#
-#             user_images = following_user.images.all()[:2]
-#
-#             for image in user_images:
-#
-#                 image_list.append(image)  #  append는 리스트 제일뒤에 추가
-#                                               ex) 리스트형식이면 리스트형식 그대로 추가
-#                                           #  extend는 리스트 확장 리스트형식의 appending
-#
-#         sorted_list = sorted(image_list, key=get_key) # 1. 어느리스트를 정렬할것인가
-#                                                       # 2. 어떻게 정렬 할것인가
-#                                                       # 3. return값을 기준으로 정렬
-#         serializer = serializers.ImageSerializer(sorted_list, many=True)
-#
-#         return Response(serializer.data)
-#
-# def get_key(image):
-#      return image.created_at
