534fe29fdb13031d5c7f6e12642b4bd8eebaa5d1
thecook <bbster12@gmail.com>
2019-10-26 20:35:59 +0900

    Image views comment 쿼리 수정
    (이후 추가 수정 예정)

--- images/views.py
+++ images/views.py
@@ -24,8 +24,8 @@ class ListAllComment(viewsets.ModelViewSet):
 
     def get(self, request, format=None):
 
-        user_id = request.user.id
-        all_comment = Comment.objects.filter(creator=user_id)  # 모든 이미지 출력
+        # user_id = request.user.id  # user id 값 불러와서 user_id에 저장
+        all_comment = Comment.objects.all()  # 모든 댓글 출력 DB에서 불러온 user id 값으로 필터링
         serializer = serializers.CommentSerializer(all_comment, many=True)
 
         return Response(data=serializer.data)
@@ -37,7 +37,7 @@ class ListAllLikes(viewsets.ModelViewSet):
     queryset = Like.objects.all()
 
     def get(self, request, format=None):
-        all_like = Like.objects.all()  # 모든 이미지 출력
+        all_like = Like.objects.all()  # 모든 좋아요 출력
 
         serializer = serializers.CommentSerializer(all_like, many=True)
 
