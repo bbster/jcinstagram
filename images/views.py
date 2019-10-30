efa010e27ec95efec6aad6e084ade4f7e28e8205
thecook <bbster12@gmail.com>
2019-10-30 20:49:30 +0900

    like image api 추가 (구현예정)

--- images/views.py
+++ images/views.py
@@ -3,45 +3,6 @@ from rest_framework.views import APIView
 from . import models, serializers
 
 
-# viewset은 list형식의 데이터 출력시 유
-# class ListAllImages(viewsets.ModelViewSet):
-#
-#     serializer_class = ImageSerializer  # 시리얼라이저
-#     queryset = Image.objects.all()  # 모델
-#
-#     # @action(detail=True, methods=['get'])
-#     def get(self, request, format=None):
-#         all_images = Image.objects.all()  # 모든 이미지 출력
-#         serializer = serializers.ImageSerializer(all_images, many=True)
-#         return Response(data=serializer.data)
-#
-#
-# class ListAllComment(viewsets.ModelViewSet):
-#
-#     serializer_class = CommentSerializer
-#     queryset = Comment.objects.all()
-#
-#     def get(self, request, format=None):
-#
-#         # user_id = request.user.id  # user id 값 불러와서 user_id에 저장
-#         all_comment = Comment.objects.all()  # 모든 댓글 출력 DB에서 불러온 user id 값으로 필터링
-#         serializer = serializers.CommentSerializer(all_comment, many=True)
-#
-#         return Response(data=serializer.data)
-#
-#
-# class ListAllLikes(viewsets.ModelViewSet):
-#
-#     serializer_class = LikeSerializer
-#     queryset = Like.objects.all()
-#
-#     def get(self, request, format=None):
-#         all_like = Like.objects.all()  # 모든 좋아요 출력
-#
-#         serializer = serializers.CommentSerializer(all_like, many=True)
-#
-#         return Response(data=serializer.data)
-
 class ListAllImages(APIView):
 
     def get(self, request, format=None):
@@ -70,7 +31,7 @@ class ListAllLikes(APIView):
 
     def get(self, request, format=None):
 
-        all_likes = models.Like.objects.all()  # like모델의 모든 오브젝트 정보를 all_likes에 저장
+        all_likes = models.Like.objects.all()  # like 모델의 모든 오브젝트 정보를 all_likes 에 저장
 
         serializer = serializers.LikeSerializer(all_likes, many=True)
 
@@ -116,3 +77,42 @@ class LikeImage(APIView):
 
         return Response(status=200)
 
+
+
+# viewset은 list형식의 데이터 출력시 유
+# class ListAllImages(viewsets.ModelViewSet):
+#
+#     serializer_class = ImageSerializer  # 시리얼라이저
+#     queryset = Image.objects.all()  # 모델
+#
+#     # @action(detail=True, methods=['get'])
+#     def get(self, request, format=None):
+#         all_images = Image.objects.all()  # 모든 이미지 출력
+#         serializer = serializers.ImageSerializer(all_images, many=True)
+#         return Response(data=serializer.data)
+#
+#
+# class ListAllComment(viewsets.ModelViewSet):
+#
+#     serializer_class = CommentSerializer
+#     queryset = Comment.objects.all()
+#
+#     def get(self, request, format=None):
+#
+#         # user_id = request.user.id  # user id 값 불러와서 user_id에 저장
+#         all_comment = Comment.objects.all()  # 모든 댓글 출력 DB에서 불러온 user id 값으로 필터링
+#         serializer = serializers.CommentSerializer(all_comment, many=True)
+#
+#         return Response(data=serializer.data)
+#
+#
+# class ListAllLikes(viewsets.ModelViewSet):
+#
+#     serializer_class = LikeSerializer
+#     queryset = Like.objects.all()
+#
+#     def get(self, request, format=None):
+#         all_like = Like.objects.all()  # 모든 좋아요 출력
+#
+#         serializer = serializers.CommentSerializer(all_like, many=True)
+#
