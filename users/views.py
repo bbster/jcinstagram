f56cba2bfa46bcc5d186668a17bc0c9e4989195e
bbster <bbster12@naver.com>
2019-11-04 18:57:33 +0900

    follow unfollow api 추가

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from . import models, serializers
 
 
-class ExploreUsers(APIView):
+class ShowUsers(APIView):
 
     def get(self, request, format=None):
 
@@ -13,3 +13,39 @@ class ExploreUsers(APIView):
         serializer = serializers.ExploreUserSerializer(last_user, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class FollowUser(APIView):  # 팔로잉
+
+    def post(self, request, user_id, format=None):
+
+        user = request.user
+
+        try:
+            user_to_follow = models.User.objects.get(id=user_id)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        user.following.add(user_to_follow)
+
+        user.save()
+
+        return Response(status=status.HTTP_200_OK)
+
+
+class UnFollowUser(APIView):  # 언팔로우
+
+    def post(self, request, user_id, format=None):
+
+        user = request.user
+
+        try:
+            user_to_follow = models.User.objects.get(id=user_id)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        user.following.remove(user_to_follow)
+
+        user.save()
+
+        return Response(status=status.HTTP_200_OK)
