9f3179296583f29fcf84c2907a3ed8c5ca7f1bf8
bbster <bbster12@naver.com>
2019-11-04 19:50:48 +0900

    ADD UserProfile API추가

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('explore/', views.ExploreUsers.as_view()),  # 모든 이미지
+    path('showusers/', views.ShowUsers.as_view()),  # 모든 유저
+    path('<int:user_id>/follow/', views.FollowUser.as_view()),  # 팔로우
+    path('<int:user_id>/unfollow/', views.UnFollowUser.as_view()),  # 언팔로우
+    path('<username>/', views.UserProfile.as_view()),  # 프로필 뷰
 ]
