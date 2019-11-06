1f74d68ca016ba0c4255820888e8c3d3d86692f7
bbster <bbster12@naver.com>
2019-11-06 14:41:13 +0900

    commit

---
+++ notifications/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users import models as user_models
+from images import models as image_models
+#  알람
+#  팔로우 알람
+#  이미지에 댓글, 좋아요 알람
+
+
+class Notification(image_models.TimeStampedModel):
+
+    TYPE_CHOICES = {
+        ('like', 'Like'),
+        ('comment', 'Comment'),
+        ('follow', 'Follow'),
+    }
+
+    creator = models.ForeignKey(user_models.User, related_name='creator', on_delete=models.CASCADE)
+    to = models.ForeignKey(user_models.User, related_name='to', on_delete=models.CASCADE)
+    notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    image = models.ForeignKey(image_models.Image, on_delete=models.CASCADE, null=True, blank=True)  # 장고 2.0부터 on_delete 명시적으로 써줘야함
