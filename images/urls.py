1bf5e9af06c7132700265c06c59191efc4cf098b
bbster <bbster12@naver.com>
2019-11-18 04:25:23 +0900

    FIX JWT token api 완료 Images/Feed views -> Images/Images views로 변경 토큰 설정 및 세팅 완료 테스트 완료

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('allimages/', views.ListAllImages.as_view()),  # 모든 이미지
     path('allcomments/', views.ListAllComments.as_view()),  # 모든 댓글
     path('alllikes/', views.ListAllLikes.as_view()),  # 모든 좋아요
-    path('feed/', views.Feed.as_view()),  # 유저관련
+    path('images/', views.Images.as_view()),  # 유저관련
     path('<int:image_id>/like/', views.LikeImage.as_view()),  # 이미지 좋아요
     path('<int:image_id>/unlike/', views.UnLikeImage.as_view()),  # 좋아요 삭제
     path('<int:image_id>/comment/', views.CommentOnImage.as_view()),  # 댓글 달기
--- images/views.py
+++ images/views.py
@@ -40,7 +40,7 @@ class ListAllLikes(APIView):
         return Response(data=serializer.data)
 
 
-class Feed(APIView):
+class Images(APIView):
 
     def get(self, request, format=None):
 
@@ -66,11 +66,24 @@ class Feed(APIView):
 
         sorted_list = sorted(image_list, key=get_key)  # 어느리스트를, 무엇을 기준으로 정렬 한다.
 
-        print(sorted_list)
-
         serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+
+        user = request.user
+
+        serializer = serializers.InputImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def get_key(image):
